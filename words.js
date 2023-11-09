88229ca4de35cfa6c0994db96cd7b57bb5457c58
2024anj <118041622+2024anj@users.noreply.github.com>
2023-11-09 11:27:05 -0500

    Update words.js

--- words.js
+++ words.js
@@ -1,4 +1,27 @@
 export const WORDS = [
-
-
+  'DEBUG',
+  'INPUT',
+  'LINUX',
+  'FLOAT',
+  'ASCII',
+  'STACK',
+  'PIXEL',
+  'SHELL',
+  'CLASS',
+  'ERROR',
+  'ARRAY',
+  'CLOUD',
+  'TOKEN',
+  'VIRUS',
+  'SCOPE',
+  'CLOCK',
+  'CACHE',
+  'MACRO',
+  'APPLE',
+  'INTEL',
+  'PROXY',
+  'ROBOT',
+  'CYBER',
+  'LOGIN',
+  'QUEUE'
 ]
